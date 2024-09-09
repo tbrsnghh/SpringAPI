af842e909942877a92f05dd19bf5c3dba711c4f1
Tbrsnghh <nhham1771@gmail.com>
2024-09-09 09:06:59 +0700

    api

---
+++ demoD060924/src/main/java/com/example/demoD060924/DemoD060924Application.java
@@ -0,0 +1,13 @@
+package com.example.demoD060924;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoD060924Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoD060924Application.class, args);
+	}
+
+}
---
+++ demoD060924/src/test/java/com/example/demoD060924/DemoD060924ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoD060924;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoD060924ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
